9c510853b47707884a375113539f41a1b96387d1
John Tribolet <john@tribolet.info>
2016-03-17 21:45:23 -0400

    builder - Add basic building logic.

--- src/builder.js
+++ src/builder.js
@@ -10,13 +10,19 @@
 module.exports = function(builder) {
     var spawn = Game.spawns.Spawn1;
 
-    if (0 === builder.energy) {
+    // console.log('Do builder');
+    if (0 === builder.carry.energy) {
+        // console.log('Builder - no energy');
         builder.moveTo(spawn);
         spawn.transferEnergy(builder);
     }
     else {
-        var site = builder.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES);
-        builder.moveTo(site);
-        builder.build(site);
+        // console.log('Builder - go to site');
+        var target = builder.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES);
+        if (target) {
+            if (builder.build(target) == ERR_NOT_IN_RANGE) {
+                builder.moveTo(target);
+            }
+        }
     }
 };
